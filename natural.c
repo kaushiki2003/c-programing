dc7b1bca4c9da21d2e96711cf22ff707af9c7f93
Kaushiki Mishra <112900571+kaushiki2003@users.noreply.github.com>
2022-10-05 10:44:32 +0530

    Add files via upload

---
+++ natural.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main(){
+    int n;
+    printf("enter any number:");
+    scanf("%d",&n);
+    if(n>=1)
+    {
+        printf("It is a natural number");
+    }
+    else
+    {
+        printf("It is not a natural number");
+    }
+    return 0;
+    
+
+}
