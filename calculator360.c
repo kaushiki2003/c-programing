4025812a2e90afff66e88d1b37d2d396b2574980
Kaushiki Mishra <112900571+kaushiki2003@users.noreply.github.com>
2022-10-08 22:56:27 +0530

    Add files via upload

---
+++ calculator360.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main(){
+    float num1,num2;
+    float result;
+    char op;
+    printf("enter the number:\n");
+    scanf("%f",&num1);
+    printf("enter the opeartor: \n");
+    scanf(" %c",&op);
+    printf("enter the number: \n ");
+    scanf("%f",&num2);
+    switch(op){
+        case'-':
+        result=num1-num2;
+        printf("%f",result);
+        break;
+        case'+':
+        result=num1+num2;
+        printf("%f",result);
+        break;
+        case'*':
+        result=num1*num2;
+        printf("%f",result);
+        break;
+        case'/':
+        result=num1/num2;
+        printf("%f",result);
+        break;
+        default:
+        printf("the operation is not valid");    
+    }
+    return 0;
+}
