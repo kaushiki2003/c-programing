fd8c5b2c61436b03bdb16b6c6d061ba611ee5f5e
Kaushiki Mishra <112900571+kaushiki2003@users.noreply.github.com>
2023-04-30 13:23:24 +0530

    Update leap.c

--- leap.c
+++ leap.c
@@ -4,7 +4,9 @@ main()
 	printf(" leap year between 1000 to 4000 \n");
 	for(i=1000;i<=4000;i++)
 	{
-		if(i%4==0)
+		      if((i%4==0 && i%100!=0)||(i%400==0))
+
+		  
 		{
 			printf("%d \n",i);
 		}
