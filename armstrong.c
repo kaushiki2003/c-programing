37bc5327fd2acb3f08ec209d9470979d7eb58493
Kaushiki Mishra <112900571+kaushiki2003@users.noreply.github.com>
2023-08-29 19:08:12 +0530

    Add files via upload

--- armstrong.c
+++ armstrong.c
@@ -1,31 +1,35 @@
-
+// C program to determine whether
+// the given number is Armstrong
+// or not
 #include <stdio.h>
 
-int main() {
-   int num, originalNum, remainder, n = 0;
-   float result = 0.0;
+// Driver code
+int main()
+{
+	int n;
 
-   printf("Enter an integer: ");
-   scanf("%d", &num);
+	printf("Enter Number \n");
+	scanf("%d", &n);
 
-   originalNum = num;
+	int var = n;
+	int sum = 0;
 
-   // store the number of digits of num in n
-   for (originalNum = num; originalNum != 0; ++n) {
-       originalNum /= 10;
-   }
+	// Loop to calculate the order of
+	// the given number
+	while (n > 0) {
+		int rem = n % 10;
+		sum = (sum) + (rem * rem * rem);
+		n = n / 10;
+	}
 
-   for (originalNum = num; originalNum != 0; originalNum /= 10) {
-       remainder = originalNum % 10;
-
-      // store the sum of the power of individual digits in result
-      result += pow(remainder, n);
-   }
-
-   // if num is equal to result, the number is an Armstrong number
-   if ((int)result == num)
-    printf("%d is an Armstrong number.", num);
-   else
-    printf("%d is not an Armstrong number.", num);
-   return 0;
+	// If the order of the number will be
+	// equal to the number then it is
+	// Armstrong number.
+	if (var == sum) {
+		printf("%d is an Armstrong number \n", var);
+	}
+	else {
+		printf("%d is not an Armstrong number", var);
+	}
+	return 0;
 }
--- armstrong2.c
+++ armstrong2.c
@@ -1,17 +1,29 @@
 #include<stdio.h>
-int main(){
-   int number,remainder,total=0,temp;
-   printf("enter the number=");
-   scanf("%d",&number);
-   temp=number;
-   while(number>0){
-      remainder=number%10;
-      total=total+(remainder*remainder*remainder);
-      number=number/10;
-   }
-   if(temp==total)
-      printf("This number is Armstrong number");
-   else
-      printf("This number is not Armstrong number");
-   return 0;
+#include<math.h>
+int main()
+{
+    int n,i,c,r,s;
+    printf("enter the nmumber:");
+    scanf("%d",n);
+    int t=n;
+    int z=n;
+    while(n!=0)
+    {
+        n=n/10;
+        c++;
+    }
+    while(t!=0)
+    {
+        r=t%10;
+        s=s+pow(r,c);
+        t=t/10;
+
+    }
+    if(s==z)
+    printf("%d is a armstrong number");
+    else
+    printf("%d is not a armstrong  number");
+    return 0;
 }
+
+
